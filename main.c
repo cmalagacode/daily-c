19dc833c9fae840ec397e644bd46ffc4e5f876f9
Cristian Malaga <cmalaga720@proton.me>
2026-02-12 17:38:45 -0500

    Quick Sort.

--- main.c
+++ main.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int binary_search(int *arr, int size, int target) {
+int partition(int arr[], int l, int h) {
+    int x = arr[h];
+    int i = (l - 1);
+    for (int j = l; j <= h - 1; j++) {
+        if (arr[j] <= x) {
+            i++;
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[h];
+    arr[h] = temp;
+    return i + 1;
+}
+
+void quick_sort_iterative(int arr[], int l, int h) {
+    int stack[h - l + 1];
+    int top = -1;
+
+    stack[++top] = l;
+    stack[++top] = h;
+
+    while (top >= 0) {
+        h = stack[top--];
+        l = stack[top--];
+
+        int p = partition(arr, l, h);
+
+        if (p - 1 > l) {
+            stack[++top] = l;
+            stack[++top] = p - 1;
+        }
+        if (p + 1 < h) {
+            stack[++top] = p + 1;
+            stack[++top] = h;
+        }
+    }
+}
+
+int binary_search(int* arr, int size, int target) {
     int low = 0;
     int high = size - 1;
     while (low <= high) {
@@ -18,12 +61,8 @@ int binary_search(int *arr, int size, int target) {
 }
 
 int main(void) {
-    int arr[] = {1, 2, 3, 4, 5, 6};
-    int i_result = binary_search(arr, 6, 2);
-    int i_result2 = binary_search(arr, 6, 3);
-    int i_result3 = binary_search(arr, 6, 5);
-    printf("%d", i_result);
-    printf("%d", i_result2);
-    printf("%d", i_result3);
+    int b[] = {4, 3, 2, 1};
+    quick_sort_iterative(b, 0, (int)(sizeof(b) / sizeof(b[0])) - 1);
+    for (int i = 0; i < 4; i++) printf("%d ", b[i]);
     return 0;
 }
