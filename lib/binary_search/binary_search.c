26ca1fe53321994e7835ef128a47cb865564e55f
Cristian Malaga <cmalaga720@proton.me>
2026-02-19 22:21:28 -0500

    add generic macro.

--- lib/binary_search/binary_search.c
+++ lib/binary_search/binary_search.c
@@ -1,6 +1,23 @@
 #include "binary_search.h"
 
-int binary_search(int* arr, int size, int target) {
+
+int32_t binary_search_i32(int32_t* arr, size_t size, int32_t target) {
+    int low = 0;
+    int high = size - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else if (arr[mid] > target) {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int8_t binary_search_i8(int8_t* arr, size_t size, int8_t target) {
     int low = 0;
     int high = size - 1;
     while (low <= high) {
--- lib/binary_search/binary_search.h
+++ lib/binary_search/binary_search.h
@@ -1,6 +1,16 @@
 #ifndef BINARY_SEARCH_H_
 #define BINARY_SEARCH_H_
 
-int binary_search(int* arr, int size, int target);
+#include <stdint.h>
+#include <stddef.h>
+
+int32_t binary_search_i32(int32_t* arr, size_t size, int32_t target);
+int8_t  binary_search_i8(int8_t* arr, size_t size, int8_t target);
+
+#define binary_search(arr, size, target) _Generic((target), \
+    int32_t: binary_search_i32, \
+    int8_t:  binary_search_i8,  \
+    default: binary_search_i32  \
+)(arr, size, target)
 
 #endif
